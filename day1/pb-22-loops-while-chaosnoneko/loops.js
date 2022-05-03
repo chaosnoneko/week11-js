59524971ef6da6704ab9c338fe2a1a77762a088d
chaosnoneko <98882297+chaosnoneko@users.noreply.github.com>
2022-05-03 11:02:30 +0200

    Update loops.js

--- day1/pb-22-loops-while-chaosnoneko/loops.js
+++ day1/pb-22-loops-while-chaosnoneko/loops.js
@@ -5,17 +5,21 @@
 let i= 0;
 while(i < 15){
     console.log(i)
+    i++;
 }
 //## 2. Add it up 
 //Create a `while` loop which sums up numbers from 1-20.
 let x=1;
+let sum =0;
 while (x<=20){
-    console.log(x)
+    sum += x
     x++;
 }
+console.log(sum)
 //## 3. Do this while i...
 //Use a `do`, `while` loop to print `The number is [i]` while i is less than 20. 
-let y=21;
+let y=0;
 do{ 
-    console.log(`The number is ` , y)
-}while(y<20)
+    console.log(`The number is  [${y}]`)
+    y++
+}while(y<20);
